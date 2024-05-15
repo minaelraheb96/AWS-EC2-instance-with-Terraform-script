c55732d5f42581884b75c79067ca0982a0f4b2f3
minaelraheb96 <minaelraheb63@gmail.com>
2024-05-15 15:39:49 +0300

    Updating Security Groups

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "Expleo-EC2-1" {
     Name = "Expleo EC2 1"
   }
 
+  vpc_security_group_ids = [aws_security_group.expleo_sg.id]
   // Making the instance have a public IP address
   associate_public_ip_address = true
 
@@ -20,4 +21,25 @@ resource "aws_instance" "Expleo-EC2-1" {
               sudo apt-get update -y
               sudo apt-get install ansible -y
               EOF
+}
+
+resource "aws_security_group" "expleo_sg" {
+
+  name        = "expleo-sg"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ 
 }
